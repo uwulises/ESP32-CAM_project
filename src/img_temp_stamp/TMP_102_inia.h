5d8684004b91149383580a61d78ffab626384440
uwulises <ulises.campodonico@ug.uchile.cl>
2022-12-01 23:26:26 -0300

    (not tested yet) add script with photo and temp stamp

---
+++ src/img_temp_stamp/TMP_102_inia.h
@@ -0,0 +1,77 @@
+#ifndef TMP_102_INIA_H
+#define TMP_102_INIA_H
+// Include the SparkFun TMP102 library.
+// Click here to get the library: http://librarymanager/All#SparkFun_TMP102
+
+#include <Wire.h> // Used to establied serial communication on the I2C bus
+#include <SparkFunTMP102.h> // Used to send and recieve specific information from our sensor
+
+// -----------------I2C-----------------
+#define I2C_SDA 14 // SDA Connected to GPIO 14
+#define I2C_SCL 15 // SCL Connected to GPIO 15
+
+float temperature;
+boolean alertPinState, alertRegisterState;
+
+TMP102 sensor0;
+
+
+void I2C_TMP102() {
+  Wire.begin(I2C_SDA, I2C_SCL);
+  /* The TMP102 uses the default settings with the address 0x48 using Wire.
+     It will return true on success or false on failure to communicate. */
+  if (!sensor0.begin(0x48))
+  {
+    Serial.println("Cannot connect to TMP102.");
+    Serial.println("Is the board connected? Is the device ID correct?");
+    while (1);
+  }
+
+  Serial.println("Connected to TMP102!");
+  delay(100);
+
+  // Initialize sensor0 settings
+  // These settings are saved in the sensor, even if it loses power
+
+  // set the number of consecutive faults before triggering alarm.
+  // 0-3: 0:1 fault, 1:2 faults, 2:4 faults, 3:6 faults.
+  sensor0.setFault(0);  // Trigger alarm immediately
+
+  // set the polarity of the Alarm. (0:Active LOW, 1:Active HIGH).
+  sensor0.setAlertPolarity(1); // Active HIGH
+
+  // set the sensor in Comparator Mode (0) or Interrupt Mode (1).
+  sensor0.setAlertMode(0); // Comparator Mode.
+
+  // set the Conversion Rate (how quickly the sensor gets a new reading)
+  //0-3: 0:0.25Hz, 1:1Hz, 2:4Hz, 3:8Hz
+  sensor0.setConversionRate(0);
+
+  //set Extended Mode.
+  //0:12-bit Temperature(-55C to +128C) 1:13-bit Temperature(-55C to +150C)
+  sensor0.setExtendedMode(0);
+}
+
+void temp_stamp() {
+
+
+  // Turn sensor on to start temperature measurement.
+  // Current consumtion typically ~10uA.
+  sensor0.wakeup();
+
+  // read temperature data
+  temperature = sensor0.readTempC();
+
+  // Place sensor in sleep mode to save power.
+  // Current consumtion typically <0.5uA.
+  sensor0.sleep();
+
+  // Print temperature and alarm state
+  Serial.print("Temperature: ");
+  Serial.println(temperature);
+  delay(1000);  // Wait 1000ms
+
+}
+
+
+#endif
---
+++ src/img_temp_stamp/img_temp_stamp.ino
@@ -0,0 +1,56 @@
+
+#include "esp_cam_inia.h"
+#include "TMP_102_inia.h"
+void setup() {
+  // put your setup code here, to run once:
+  // Disable brownout detector
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+
+  // Start Serial Monitor
+  Serial.begin(115200);
+
+  //Init TMP102 I2C
+  I2C_TMP102();
+
+  // Initialize the camera
+  Serial.print("Initializing the camera module...");
+  configESPCamera();
+  Serial.println("Camera OK!");
+
+  // Initialize the MicroSD
+  Serial.print("Initializing the MicroSD card module... ");
+  initMicroSDCard();
+
+  // initialize EEPROM with predefined size
+  // EEPROM.begin(EEPROM_SIZE);
+  //pictureCount = EEPROM.read(0) + 1;
+  pictureCount = + 1;
+
+  // Path where new picture will be saved in SD Card
+  String path = "/image" + String(pictureCount) + ".jpg";
+  Serial.printf("Picture file name: %s\n", path.c_str());
+
+  // Take and Save Photo
+  takeNewPhoto(path);
+
+  // Update EEPROM picture number counter
+  //EEPROM.write(0, pictureCount);
+  // EEPROM.commit();
+
+  // Bind Wakeup to GPIO13 going LOW
+  esp_sleep_enable_ext0_wakeup(GPIO_NUM_13, 0);
+
+  Serial.println("Entering sleep mode");
+  delay(1000);
+
+  // Enter deep sleep mode
+  esp_deep_sleep_start();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if (digitalRead(GPIO_NUM_13)){
+    temp_stamp();}
+
+}
